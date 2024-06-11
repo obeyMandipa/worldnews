324693925b6a8a05dcb3fcde639d3730a0e7efab
Rufaro O Mandipa <mandipaobey@gmail.com>
2024-06-11 22:58:09 +0200

    initial commit

---
+++ styles.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    header: {
+      flex: 1,
+      backgroundColor: '#fff',
+      marginTop: 60,
+      alignItems: 'center'
+    },
+  });
+
+  export default styles
